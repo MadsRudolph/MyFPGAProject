25cf356c949b08ab7ffb5954e9d3c51fd48a8568
Mads Vølkers Rudolph <s246132@dtu.dk>
2025-03-31 15:56:16 +0200

    Progress at school

--- Calc_Projekt/Calc_Menu.vhd
+++ Calc_Projekt/Calc_Menu.vhd
@@ -43,12 +43,44 @@ entity Calc_Menu is
            Done : in  STD_LOGIC;
            OpCode : out  STD_LOGIC_VECTOR (2 downto 0);
            In1 : out  STD_LOGIC_VECTOR (7 downto 0);
-           In2 : out  STD_LOGIC_VECTOR (7 downto 0));
+           In2 : out  STD_LOGIC_VECTOR (7 downto 0);
+			  SW: in STD_LOGIC_VECTOR (7 downto 0));
 end Calc_Menu;
 
 architecture Behavioral of Calc_Menu is
 
+type Statetype is (A,B,C,D,E,F,G,H,I, Val1, Op1, Val2, Op2, Val3);
+
+signal state, nextstate : Statetype;
+
+    -- Internal signals
+    signal Op1S, Op2S : STD_LOGIC_VECTOR (2 downto 0);
+	 signal Val2S: STD_LOGIC_VECTOR (7 downto 0);
+    signal Val1S, Val3S, DisplayData : STD_LOGIC_VECTOR (15 downto 0);
+    signal Val1En, Val2En, Val3En, Op1En, Op2En : STD_LOGIC;
+
 begin
+
+Statereg: process(reset, clk)
+begin 
+        if reset = '1' then 
+            state <= A;
+        elsif clk'event and clk = '1' then
+            state <= NextState;
+        end if;
+    end process;
+	 
+StateDec: process (state, func, enter, operation, done)
+	begin
+		
+
+
+Val1Reg: entity work.std_8bit_reg port map(reset, clk, SW);
+Val2Reg: entity work.std_8bit_reg port map(reset, clk, SW);
+Val3Reg: entity work.std_8bit_reg port map(reset, clk, SW);
+Op1Reg: entity work.std_8bit_reg port map(reset, clk, SW);
+Op2Reg: entity work.std_8bit_reg port map(reset, clk, SW);
+CalcValReg: entity work.std_8bit_reg port map(reset, clk);
 
 
 end Behavioral;
