2c5df8a8ec2c5b90ddfb7a319ef3272700670af2
Mads <Mads28122001@hotmail.dk>
2025-05-05 15:28:41 +0200

    Added PWM-VHDL to repo

---
+++ PWM - VHDL/TopLys/Sawtooth.vhd
@@ -0,0 +1,37 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:32:05 05/05/2025 
+-- Design Name: 
+-- Module Name:    Sawtooth - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEE.STD_LOGIC_unsigned.ALL;
+
+entity Sawtooth is
+    Port ( Reset : in  STD_LOGIC;
+           Clk : in  STD_LOGIC;
+           SigAmpl : out  STD_LOGIC);
+end Sawtooth;
+
+architecture Behavioral of Sawtooth is
+
+begin
+
+Sawtooth: process()
+
+end Behavioral;
+
