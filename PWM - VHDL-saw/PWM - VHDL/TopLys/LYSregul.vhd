ddea37fc126c740fa33c2e67f42f7d685c4597b9
Mads <Mads28122001@hotmail.dk>
2025-05-08 22:46:08 +0200

    minor fixes, for rapport

--- PWM - VHDL-saw/PWM - VHDL/TopLys/LYSregul.vhd
+++ PWM - VHDL-saw/PWM - VHDL/TopLys/LYSregul.vhd
@@ -1,16 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_unsigned.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity LYSregul is
     Port ( Reset : in  STD_LOGIC;
            MClk : in  STD_LOGIC;
